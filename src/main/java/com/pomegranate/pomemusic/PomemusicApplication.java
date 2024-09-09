e7fcc077a9be7b48cb7c4e4373ce6b7929cfd190
GeorgeU2030 <georgeu2021@gmail.com>
2024-09-09 10:55:30 -0500

    setup project spring boot

---
+++ src/main/java/com/pomegranate/pomemusic/PomemusicApplication.java
@@ -0,0 +1,13 @@
+package com.pomegranate.pomemusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PomemusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PomemusicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pomegranate/pomemusic/PomemusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pomegranate.pomemusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PomemusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
